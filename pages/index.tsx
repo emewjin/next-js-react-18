aad2bb4a75f0034359a4e8551eb8a30e666e267b
emewjin <emewjin@gmail.com>
2022-02-02 22:24:01 +0900

    tailwind css 초기세팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return null;
-}
+  return (
+    <div className="bg-red-100">
+      <h1 className="text-blue-500">hi</h1>
+      <p>hi</p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
